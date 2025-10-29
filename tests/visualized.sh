721d81ec8c13dd70c9a46648038ea529cf64da6e
Florian Loitsch <florian@toit.io>
2025-10-29 19:56:29 +0100

    Update toit-png-tools and tests. (#111)

--- tests/visualized.sh
+++ tests/visualized.sh
@@ -10,6 +10,6 @@ TOIT_PROGRAM=$2
 
 mkdir -p tests/out
 
-$TOIT_EXE -Xenable_asserts tests/$TOIT_PROGRAM tests/out/$TOIT_PROGRAM
+$TOIT_EXE run -Xenable_asserts tests/$TOIT_PROGRAM tests/out/$TOIT_PROGRAM
 
 $TOIT_EXE tests/toit-png-tools/bin/pngdiff.toit -o tests/out/diff-$TOIT_PROGRAM.png -t tests/out/$TOIT_PROGRAM.png tests/gold/$TOIT_PROGRAM.png
